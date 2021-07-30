5e4f5d3043e0e34d6b20e2359c15da4d335f5798
Erkebulan <71064146+Erkebnulan@users.noreply.github.com>
2021-07-30 19:57:03 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//show-hide menu box
+$('.call-menu').click(function () {
+    $('.menu-box').addClass('show');
+})
+$('.close-menu-box').click(function () {
+    $('.menu-box').removeClass('show');
+})
+
+//
